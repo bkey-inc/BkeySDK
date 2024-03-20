4a70a8e18bff5c9e0d57e3925f17fcdff4ffdebe
Loco-xD <david@shack15.com>
2024-03-20 16:40:39 -0500

    Init

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BkeySDK",
+    platforms: [
+      .iOS(.v16),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "BkeySDK",
+            targets: ["BkeySDK"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/GigaBitcoin/secp256k1.swift.git", exact: "0.15.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "BkeySDK"),
+        .testTarget(
+            name: "BkeySDKTests",
+            dependencies: ["BkeySDK"]),
+        .binaryTarget(
+            name: "Bkey",
+            path: "Bkey.xcframework"
+        )
+    ]
+)

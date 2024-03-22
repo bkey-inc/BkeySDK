ab60e371fd9295c3a1dc3c54b8b0beba7f69c99b
Loco-xD <david@shack15.com>
2024-03-22 08:37:36 -0500

    Updating target

--- Package.swift
+++ Package.swift
@@ -20,14 +20,14 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
+        /*.target(
             name: "BkeySDK"),
         .testTarget(
             name: "BkeySDKTests",
-            dependencies: ["BkeySDK"]),
+            dependencies: ["BkeySDK"]),*/
         .binaryTarget(
-            name: "Bkey",
-            path: "Bkey.xcframework"
+            name: "BkeySDK",
+            path: "BkeySDK.xcframework"
         )
     ]
 )
